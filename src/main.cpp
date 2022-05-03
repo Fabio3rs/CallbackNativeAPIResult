dc03640149855b33cef30b36c3dd494f6cf345c1
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2022-05-03 18:44:27 -0300

    Adjusting types

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,10 @@ int main() {
 typedef int (*callbackcwd_t)(uintptr_t code, const char *workingdirectory,
                              size_t maxsize);
 
+/*
+Simulates the idea of an working directory function that sends the result with a
+callback
+*/
 int getcwdcb(uintptr_t code, callbackcwd_t fn) { return fn(code, "/home", 0); }
 
 typedef int (*systemStartupCallback_t)(uintptr_t code, const char *json,
@@ -33,6 +37,27 @@ int inicializar(const char *config, uintptr_t code,
                 systemStartupCallback_t fn) {
     std::cout << config << std::endl;
     return fn(code, R"({ "success": true })", 0);
+}
+
+static void exampleWorkingDirectory() {
+    using namespace WrapperCallbackCode;
+    std::string WorkingDirectory;
+
+    {
+        ScopedCallbackMgr<int, const char *, size_t> cb(
+            [&WorkingDirectory](const char *workingdirectory, size_t maxsize) {
+                size_t wkdirLen = maxsize == 0
+                                      ? strlen(workingdirectory)
+                                      : strnlen(workingdirectory, maxsize);
+                WorkingDirectory.assign(workingdirectory, wkdirLen);
+                return 0;
+            });
+
+        getcwdcb(cb.getCode(), decltype(cb)::callbackWrapper);
+    }
+
+    std::cout << "Supondo que este seja um working directory "
+              << WorkingDirectory << std::endl;
 }
 
 static void exampleScopedCallback() {
@@ -61,39 +86,18 @@ static void exampleDirectCallback() {
     std::string SystemStartupResult;
 
     {
-        DirectCallbackMgr cb((std::function<int(const char *, size_t)>(
+        DirectCallbackMgr<int, const char *, size_t> cb(
             [&SystemStartupResult](const char *json, size_t maxsize) {
                 size_t wkdirLen =
                     maxsize == 0 ? strlen(json) : strnlen(json, maxsize);
                 SystemStartupResult.assign(json, wkdirLen);
                 return 0;
-            })));
+            });
 
         inicializar(R"({})", cb.getCode(), decltype(cb)::callbackWrapper);
     }
 
     std::cout << "AAAAA " << SystemStartupResult << std::endl;
-}
-
-static void exampleWorkingDirectory() {
-    using namespace WrapperCallbackCode;
-    std::string WorkingDirectory;
-
-    {
-        ScopedCallbackMgr cb((std::function<int(const char *, size_t)>(
-            [&WorkingDirectory](const char *workingdirectory, size_t maxsize) {
-                size_t wkdirLen = maxsize == 0
-                                      ? strlen(workingdirectory)
-                                      : strnlen(workingdirectory, maxsize);
-                WorkingDirectory.assign(workingdirectory, wkdirLen);
-                return 0;
-            })));
-
-        getcwdcb(cb.getCode(), decltype(cb)::callbackWrapper);
-    }
-
-    std::cout << "Supondo que este seja um working directory "
-              << WorkingDirectory << std::endl;
 }
 
 int main() {
